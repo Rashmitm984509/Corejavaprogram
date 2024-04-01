0f88dbd7ef7705a8437db7e2b144d50eb23e9745
TNS INDIA <TNS INDIA@LAPTOP-N0JHV8G0>
2024-04-01 20:31:26 +0530

    First commit

--- corejavaprogram/src/com/tnsif/practice/Firstprogram.java
+++ corejavaprogram/src/com/tnsif/practice/Firstprogram.java
@@ -1,9 +1,23 @@
 package com.tnsif.practice;
 
 public class Firstprogram {
-    int  a=9;
+  public   int  a=9;// data member
+    private float g=9.6f;
+    protected int s=5;
+  
+  int d=3;
+    void main() { // member function
+    	
+    }
 	public static void main(String[] args) {
-		System.out.println("welcome to java");
+		//System.out.println("welcome to java");
+		Firstprogram f=new Firstprogram();
+		
+		System.out.println(f.a);
+		System.out.println(f.g);
+		System.out.println(f.s);
+		System.out.println(f.d);
+
 
 	}
 
