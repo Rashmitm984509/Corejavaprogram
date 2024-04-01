6549f1847fa678ad165bcecd9878408c5a8f3bce
TNS INDIA <TNS INDIA@LAPTOP-N0JHV8G0>
2024-04-01 20:32:50 +0530

    First commit

---
+++ corejavaprogram/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+// to demostarte encapsulation
+public class EncapsulationDemo {
+  
+	int n;   // data members
+	String s;
+	int a;
+	
+	public void show() {     // member function
+		System.out.println("Serialnumber"+ n +"\t name "+  s+"\t age" +a);
+	}
+}
---
+++ corejavaprogram/src/com/tnsif/practice/PublicDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.practice;
+
+public class PublicDemo {
+public static void main(String[] args) {
+	Firstprogram f=new Firstprogram();
+	System.out.println(f.a);
+	//System.out.println(f.g);
+	System.out.println(f.s);
+	System.out.println(f.d);
+
+}
+}
