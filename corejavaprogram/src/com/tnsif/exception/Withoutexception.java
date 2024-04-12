b6c07662aefddea7bec35d464d2e5463320f1f16
TNS INDIA <TNS INDIA@LAPTOP-N0JHV8G0>
2024-04-12 12:17:51 +0530

    First commit

---
+++ corejavaprogram/src/com/tnsif/exception/Withoutexception.java
@@ -0,0 +1,17 @@
+package com.tnsif.exception;
+
+public class Withoutexception {
+	
+public static void main(String[] args) {
+	int d=0;
+	System.out.println("hello");
+	
+	try {
+	int a=55/0;
+	}
+	catch(ArithmeticException e) {
+		System.out.println("exception caught");
+	}
+	
+}
+}
---
+++ corejavaprogram/src/com/tnsif/inheritance/Mother.java
@@ -0,0 +1,7 @@
+package com.tnsif.inheritance;
+
+public class Mother {
+void main() {
+	System.out.println("ras");
+}
+}
